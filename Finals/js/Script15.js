3fbc36678dc0ceeb7a6b4f03e0832671fa882522
Shyrlee Tyfanny A. Pejo <20180555@s.ubaguio.edu>
2022-05-22 00:18:43 +0700

    Update Script15.js

---
+++ Finals/js/Script15.js
@@ -0,0 +1,18 @@
+const NAME = "Shyrlee Tyfanny A. Pejo";
+const AGE = 22;
+const NUM = "(+63)" + 9294896106;
+const EMAIL = "20180555@s.ubaguio.edu";
+const MADDRESS = "Brgy. Ronda Sta. Lucia Ilocos Sur";
+const PROGRAM = "Information Technology";
+const IDNUM = 20180555;
+const YEARLEVEL = "2nd Year";
+const SECTION = "IAA1";
+
+Document.getElementById("firstname").innerHTML = NAME;
+Document.getElementById("age").innerHTML = AGE;
+Document.getElementById("phonenumber").innerHTML = NUM;
+Document.getElementById("emailaddress").innerHTML = MADDRESS;
+Document.getElementById("program").innerHTML = PROGRAM;
+Document.getElementById("idnumber").innerHTML = IDNUM;
+Document.getElementById("yearlevel").innerHTML = YEARLEVEL;
+Document.getElementById("section").innerHTML = SECTION;
